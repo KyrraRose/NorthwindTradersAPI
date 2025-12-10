d162d0aeb27e75864100a9b082116ff8fe4b1c5f
KyrraRose <krbosland1@gmail.com>
2025-12-10 10:30:35 -0500

    first commit

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -0,0 +1,15 @@
+package com.pluralsight.NorthwindTradersAPI;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(path="/", method= RequestMethod.GET)
+    public String index(@RequestParam(defaultValue="World!") String country) {
+        return "Hello " + country;
+    }
+
+
+
+}
